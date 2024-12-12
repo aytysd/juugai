d5fd0866874667a4d3ee56c1b53bd359615b8ba4
DESKTOP-NE7RMMQ\aytys <I10795@nara.kosen-ac.jp>
2024-12-12 17:01:12 +0900

    changes

--- analyzer-node/src/dist/components/cordinate/PolarCord.js
+++
@@ -1,6 +0,0 @@
-export function convertPolar2Orth(p) {
-    const x = p.r * Math.cos(p.theta);
-    const y = p.r * Math.sin(p.theta);
-    return { x, y };
-}
-//# sourceMappingURL=PolarCord.js.map
